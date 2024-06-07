567f0226ddacaae0aa3777679864ac353d089f83
Tibo <129294210+tibo44@users.noreply.github.com>
2024-06-07 16:54:52 +0200

    Initial commit

---
+++ src/main/java/opium44/testmod/TestModDataGenerator.java
@@ -0,0 +1,11 @@
+package opium44.testmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TestModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
